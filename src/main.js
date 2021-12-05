6e95e72a9b97c4d234f942a6c8e062befa317075
rullyafrizal <rullyafrizal4@gmail.com>
2021-12-06 00:34:46 +0700

    moving scss to css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/index.css'
-import './assets/css/index.scss'
 import store from './store'
 import axios from './plugins/axios'
 import router from './router'
